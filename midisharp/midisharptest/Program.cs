20f0c975285dbc29e9e17767752e3126df378339
bnoden <brandroid369@gmail.com>
2017-05-08 03:23:11 -0500

    .

---
+++ midisharp/midisharptest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace midisharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ midisharp/wavtest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.Devices;
+
+namespace Wavtest {
+    class Program {
+       static Audio wa = new Audio();
+        
+        private static byte[] wavdata;
+        
+
+        static void Main(string[] args) {
+            //var curdir = Directory.GetCurrentDirectory();
+            string dirFX = $"{Directory.GetCurrentDirectory()}\\res\\", strJump = "Jump.wav";
+
+            wavdata = File.ReadAllBytes($"{dirFX}{strJump}");
+
+            WriteLine($"Playing {strJump}\nrepeat=true");
+            
+            int count = 5;
+            while (count>0) {
+                wa.Play(wavdata, AudioPlayMode.WaitToComplete);
+                count--;
+            }
+
+
+        }
+    }
+}
