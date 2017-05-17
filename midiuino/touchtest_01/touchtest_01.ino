3a07bbf974f2c429895cfdc05bfd58c0e2a615b6
bnoden <brandroid369@gmail.com>
2017-05-17 01:10:09 -0500

    .

--- midiuino/touchtest_01/touchtest_01.ino
+++ midiuino/touchtest_01/touchtest_01.ino
@@ -1,28 +1,17 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-const int x1 = A0;
-const int y2 = A1;
-const int x2 = A2;
-const int y1 = A3;
+const uint16_t x1 = A0;
+const uint16_t y2 = A1;
+const uint16_t x2 = A2;
+const uint16_t y1 = A3;
 
 void setup() {
   lcd.begin(16,2);
   lcd.clear();
 }
 
-int readX() {
-  pinMode(y1, INPUT);
-  pinMode(x2, OUTPUT);
-  pinMode(y2, INPUT);
-  pinMode(x1, OUTPUT);
-  digitalWrite(x2, LOW);
-  digitalWrite(x1, HIGH);
-  delay(5);
-  return analogRead(y1);
-}
-
-int readY() {
+uint16_t readX() {
   pinMode(y1, OUTPUT);
   pinMode(x2, INPUT);
   pinMode(y2, OUTPUT);
@@ -33,12 +22,21 @@ int readY() {
   return analogRead(x2);
 }
 
+uint16_t readY() {
+  pinMode(y1, INPUT);
+  pinMode(x2, OUTPUT);
+  pinMode(y2, INPUT);
+  pinMode(x1, OUTPUT);
+  digitalWrite(x2, LOW);
+  digitalWrite(x1, HIGH);
+  delay(5);
+  return analogRead(y1);
+}
+
 void loop() {
-  int y = readY();
-  int x = readX();
   lcd.setCursor(0,0);
-  lcd.print("X ");lcd.print(x);
+  lcd.print("X ");lcd.print(readX());
   lcd.setCursor(0,1);
-  lcd.print("Y ");lcd.print(y);
+  lcd.print("Y ");lcd.print(readY());
   delay (200);
 }
